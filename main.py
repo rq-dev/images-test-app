a84e37c6c638d92cedfbf4298e4319d7102932a6
yromkaq@mail.ru <yromkaq@mail.ru>
2021-11-07 19:03:46 +0500

    image import and save to bmp

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ from platform import system
 from tkinter.filedialog import askopenfilename
 from PIL import ImageTk, Image
 
-
 FIRST_IMAGE = ""
 SECOND_IMAGE = ""
-
 
 platformD = system()
 if platformD == 'Darwin':
@@ -25,21 +23,31 @@ else:
 
 def openFirstImage():
     global FIRST_IMAGE
-    canvas1 = Canvas(window, height=512, width=512)
-    canvas1.pack()
+    canvas1 = Canvas(window, height=300, width=300)
+    canvas1.place(relx=0.1, rely=0.1)
     FIRST_IMAGE = askopenfilename()
-    first = ImageTk.PhotoImage(Image.open(FIRST_IMAGE))
+    first = ImageTk.PhotoImage(Image.open(FIRST_IMAGE).resize((300, 300), Image.ANTIALIAS))
     window.first = first
     canvas1.create_image((0, 0), anchor="nw", image=first)
 
 
 def openSecondImage():
     global SECOND_IMAGE
+    canvas1 = Canvas(window, height=300, width=300)
+    canvas1.place(relx=0.6, rely=0.1)
     SECOND_IMAGE = askopenfilename()
+    second = ImageTk.PhotoImage(Image.open(SECOND_IMAGE).resize((300, 300), Image.ANTIALIAS))
+    window.second = second
+    canvas1.create_image((0, 0), anchor="nw", image=second)
 
 
 def show_about():
     messagebox.showinfo("Made By Roman Yaschenko")
+
+
+def saveImageToBMP(img):
+    picture = Image.open(img)
+    picture = picture.save("{}.bmp".format(img))
 
 
 counter = 0
@@ -53,12 +61,21 @@ button1 = Button(text="First Image",
                  font="16",
                  justify="center",
                  command=openFirstImage)
-button1.place(relx=0.2, rely=0.7, anchor="w", width=130, bordermode=OUTSIDE)
+button1.place(relx=0.2, rely=0.7, anchor="w", width=110, bordermode=OUTSIDE)
+button_save_bmp_1 = Button(text="First Image",
+                           font="16",
+                           justify="center",
+                           command=lambda: saveImageToBMP(FIRST_IMAGE))
+button_save_bmp_1.place(relx=0.2, rely=0.8, anchor="w", width=110, bordermode=OUTSIDE)
 button2 = Button(text="Second Image",
                  font="16",
                  justify="center",
                  command=openSecondImage)
-button2.place(relx=0.8, rely=0.7, anchor="e", width=130, bordermode=OUTSIDE)
-
+button2.place(relx=0.8, rely=0.7, anchor="e", width=110, bordermode=OUTSIDE)
+button_save_bmp_2 = Button(text="SAVE",
+                           font="16",
+                           justify="center",
+                           command=lambda: saveImageToBMP(SECOND_IMAGE))
+button_save_bmp_2.place(relx=0.7, rely=0.8, anchor="w", width=110, bordermode=OUTSIDE)
 window.config(menu=main_menu)
 window.mainloop()
