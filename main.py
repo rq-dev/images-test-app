7207c9d91c79713fbc8e136c9f172ddf23214dc8
yromkaq@mail.ru <yromkaq@mail.ru>
2021-11-07 15:57:49 +0500

    image

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from tkinter import *
 from tkinter import messagebox
 from platform import system
+from tkinter.filedialog import askopenfilename
+from PIL import ImageTk, Image
+
+
+FIRST_IMAGE = ""
+SECOND_IMAGE = ""
 
 
 platformD = system()
@@ -17,14 +23,23 @@ else:
     app_icon = "icons/icons8-image.ico"
 
 
-def increase_counter():
-    global counter
-    counter += 1
-    window.title("Image Test App {}".format(counter))
+def openFirstImage():
+    global FIRST_IMAGE
+    canvas1 = Canvas(window, height=512, width=512)
+    canvas1.pack()
+    FIRST_IMAGE = askopenfilename()
+    first = ImageTk.PhotoImage(Image.open(FIRST_IMAGE))
+    window.first = first
+    canvas1.create_image((0, 0), anchor="nw", image=first)
+
+
+def openSecondImage():
+    global SECOND_IMAGE
+    SECOND_IMAGE = askopenfilename()
 
 
 def show_about():
-    messagebox.showinfo("GUI Python")
+    messagebox.showinfo("Made By Roman Yaschenko")
 
 
 counter = 0
@@ -37,12 +52,12 @@ main_menu.add_cascade(label="About", command=show_about)
 button1 = Button(text="First Image",
                  font="16",
                  justify="center",
-                 command=increase_counter)
+                 command=openFirstImage)
 button1.place(relx=0.2, rely=0.7, anchor="w", width=130, bordermode=OUTSIDE)
 button2 = Button(text="Second Image",
                  font="16",
                  justify="center",
-                 command=increase_counter)
+                 command=openSecondImage)
 button2.place(relx=0.8, rely=0.7, anchor="e", width=130, bordermode=OUTSIDE)
 
 window.config(menu=main_menu)
