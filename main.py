a44cf86aacef51df4bbc6ba7d732f8aa63fc0fd4
yromkaq@mail.ru <yromkaq@mail.ru>
2022-01-23 20:57:33 +0500

    median cut fixed

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tkinter import messagebox
 from platform import system
 from tkinter.filedialog import askopenfilename
 from PIL import ImageTk, Image
-from math import log10, sqrt
+from math import log10, sqrt, log
 import cv2
 import numpy as np
 import datetime
@@ -259,12 +259,69 @@ def toRGB(img, c):
         canvas1.create_image((0, 0), anchor="nw", image=first)
 
 
+def median_cut_quantize(img, img_arr):
+    # when it reaches the end, color quantize
+    r_average = np.mean(img_arr[:, 0])
+    g_average = np.mean(img_arr[:, 1])
+    b_average = np.mean(img_arr[:, 2])
+
+    for data in img_arr:
+        img[data[3]][data[4]] = [r_average, g_average, b_average]
+
+
+def split_into_buckets(img, img_arr, depth):
+    if len(img_arr) == 0:
+        return
+
+    if depth == 0:
+        median_cut_quantize(img, img_arr)
+        return
+
+    r_range = np.max(img_arr[:, 0]) - np.min(img_arr[:, 0])
+    g_range = np.max(img_arr[:, 1]) - np.min(img_arr[:, 1])
+    b_range = np.max(img_arr[:, 2]) - np.min(img_arr[:, 2])
+
+    space_with_highest_range = 0
+
+    if g_range >= r_range and g_range >= b_range:
+        space_with_highest_range = 1
+    elif b_range >= r_range and b_range >= g_range:
+        space_with_highest_range = 2
+    elif r_range >= b_range and r_range >= g_range:
+        space_with_highest_range = 0
+
+    # sort the image pixels by color space with highest range
+    # and find the median and divide the array.
+    img_arr = img_arr[img_arr[:, space_with_highest_range].argsort()]
+    median_index = int((len(img_arr) + 1) / 2)
+
+    # split the array into two blocks
+    split_into_buckets(img, img_arr[0:median_index], depth - 1)
+    split_into_buckets(img, img_arr[median_index:], depth - 1)
+
+
 def doMC(img):
     setCOLOR()
     global FIRST_IMAGE
     original = img.copy()
-    img = img.quantize(COLORS, 0)
-    FIRST_IMAGE = img
+    saveImageWithName(img, 'bmp', 'tempmc')
+    sample_img = io.imread('tempmc.bmp')
+    deleteImage('tempmc.bmp')
+    flattened_img_array = []
+    colors = round(log(COLORS, 2))
+    for rindex, rows in enumerate(sample_img):
+        for cindex, color in enumerate(rows):
+            flattened_img_array.append([color[0], color[1], color[2], rindex, cindex])
+
+    flattened_img_array = np.array(flattened_img_array)
+
+    # start the splitting process
+    split_into_buckets(sample_img, flattened_img_array, colors)
+    io.imsave('res.bmp', sample_img)
+    image_final = Image.open('res.bmp')
+    deleteImage("res.bmp")
+
+    FIRST_IMAGE = image_final
     canvas1 = Canvas(window, height=512, width=512)
     canvas1.grid(row=0, column=1, padx=5, pady=5, columnspan=2, rowspan=10, sticky="e")
     first = ImageTk.PhotoImage(FIRST_IMAGE.resize((512, 512), Image.ANTIALIAS))
